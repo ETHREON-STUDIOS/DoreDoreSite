cd2ba01e94a7b80fd2e055d19bfda23fae8bc390
Muhammed Emir Tohumcu <muhammedemirtohumcu@gmail.com>
2025-04-26 13:56:49 +0300

    Home Page Added

--- DoreDoreWeb/DoreDoreWeb/obj/Debug/net8.0/DoreDoreWeb.AssemblyInfo.cs
+++ DoreDoreWeb/DoreDoreWeb/obj/Debug/net8.0/DoreDoreWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoreDoreWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51a5099d08c7345914712babef565bd05dcabc9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoreDoreWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoreDoreWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
